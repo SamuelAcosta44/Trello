6d5f7f0222325f5504657f426163b44513dccda3
SamuelAcosta44 <samuelacosta@ticsocial.com.co>
2023-12-12 11:49:58 -0500

    Crear campo 'status' con choices

--- apirest/models.py
+++ apirest/models.py
@@ -2,7 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
+    STATUS_NAME = [
+        ('BACKLOG', 'Backlog'),
+        ('TO DO', 'To Do'),
+        ('DOING', 'Doing'),
+        ('TEST', 'Test'),
+        ('DONE', 'Done')
+    ]
+
     name = models.CharField(max_length=40)
     description = models.TextField()
     deadline = models.DateField()
     comment = models.TextField()
+    status = models.CharField(max_length = 10, choices = STATUS_NAME)
+
+    def __str__(self):
+        return self.name
