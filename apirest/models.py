1b3381022a477a6f2139ce5cd0001622db042d79
SamuelAcosta44 <samuelacosta@ticsocial.com.co>
2023-12-12 12:10:00 -0500

    Se crea el modelo prioridad y se relaciona con el de Tareas

--- apirest/models.py
+++ apirest/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Priority(models.Model):
+    PRIORITY_NAME = [
+        ('HIGH', 'High'),
+        ('MEDIUM', 'Medium'),
+        ('LOW', 'Low')
+    ]
+    name = models.CharField(max_length=40, choices=PRIORITY_NAME)
+
+    def __str__(self):
+        return self.name
+
+
 class Task(models.Model):
     STATUS_NAME = [
         ('BACKLOG', 'Backlog'),
@@ -15,6 +27,7 @@ class Task(models.Model):
     deadline = models.DateField()
     comment = models.TextField()
     status = models.CharField(max_length = 10, choices = STATUS_NAME, default = 'Backlog')
+    priority = models.ForeignKey(Priority, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
