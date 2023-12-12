196b5ef60571b567a90cfe528a39b24d4fd855ee
SamuelAcosta44 <samuelacosta@ticsocial.com.co>
2023-12-12 17:01:57 -0500

    Se crea el POST

--- apirest/urls.py
+++ apirest/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getTasks)
+    path('task/', views.getTasks),
+    path('task/create/', views.postTask),
 ]
--- apirest/views.py
+++ apirest/views.py
@@ -9,3 +9,10 @@ def getTasks(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def postTask(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
