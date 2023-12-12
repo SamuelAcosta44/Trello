5e5bc4b1ef09d6901e314af6ca152e70c3360891
SamuelAcosta44 <samuelacosta@ticsocial.com.co>
2023-12-12 11:17:40 -0500

    Se crea el modelo principal de tareas

--- apirest/models.py
+++ apirest/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    deadline = models.DateField()
+    comment = models.TextField()
