fdcef06eec6db48a7db7405326385d7524b5ed49
SamuelAcosta44 <samuelacosta@ticsocial.com.co>
2023-12-15 11:20:04 -0500

    Se crea el metodo PUT

--- apirest/views.py
+++ apirest/views.py
@@ -22,3 +22,17 @@ def addTask(request):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+@api_view(['PUT'])
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, date=request.data)
+
+    try:
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
